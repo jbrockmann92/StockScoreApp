82ec5f7ad58fed4f34ad12bd10b2975a4edf3d5b
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-03-16 11:45:30 -0500

    Added Searches table

---
+++ StockScore/Models/Searches.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockScore.Models
+{
+    public class Searches
+    {
+        [Key]
+        int Id { get; set; }
+        string Ticker { get; set; }
+        float Score { get; set; }
+        string DateSearched { get; set; }
+
+    }
+}
