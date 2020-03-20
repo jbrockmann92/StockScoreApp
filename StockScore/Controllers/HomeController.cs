a46d0993efc213ec1b752b30ad125d72a490d526
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-03-20 06:52:30 -0700

    Finished method to sort stocks and return the four with the highest scores. Testing now

--- StockScore/Controllers/HomeController.cs
+++ StockScore/Controllers/HomeController.cs
@@ -28,11 +28,12 @@ namespace StockScore.Controllers
         {
             Scoring scoring = new Scoring();
             Top_Stocks top_Stocks = new Top_Stocks();
-            List<string> unsortedStocks = new List<string>();
+            List<Searches> unsortedStocks = new List<Searches>();
             
 
             //Will need to change once I've implemented time frame, but works for now
 
+            //May be better in the Startup class, but works here for now
 
             var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
             if (userId == null)
@@ -41,11 +42,12 @@ namespace StockScore.Controllers
                 {
                     Searches search = new Searches();
                     search.Symbol = top_Stocks.possibleTopStocks[i];
-                    int stockScore = scoring.GetStockScore(search);
-
+                    search.Score = scoring.GetStockScore(search);
+                    unsortedStocks.Add(search);
                 }
 
-                List<string> sortedStocks = SortStocks();
+                List<string> sortedStocks = SortStocks(unsortedStocks);
+                //By this point, sortedStocks should contain the four strings that are the stock symbols for the highest scoring stocks
 
                 return Redirect("./Identity/Account/Login");
             }
@@ -67,9 +69,18 @@ namespace StockScore.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public List<string> SortStocks()
+        public List<string> SortStocks(List<Searches> stocks)
         {
+            List<string> sortedStocks = new List<string>();
 
+            List<Searches> sortedSearches = stocks.OrderByDescending(s => s.Score).ToList();
+
+            for (int i = 0; i < 4; i++)
+            {
+                sortedStocks.Add(sortedSearches[i].Symbol.ToUpper());
+            }
+
+            return sortedStocks;
         }
     }
 }
